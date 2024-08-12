1ec0a54a6f75772133fc2c426492a8292209a661
Ahmed-Yacine Salmi <149178831+salmiahmedyacine@users.noreply.github.com>
2024-08-12 14:54:52 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*! With -Ahmed Yacine Salmi- from Abdelrahman Gamal */
+
+let img = document.querySelector('.img');
+let container = document.getElementById('container');   
+function phones(phone) {
+    img.src = phone;  
+}
+
+function colors(color){
+    container.style.background = color;
+}
